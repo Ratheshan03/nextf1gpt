6abf38ca5e96c5b96a59919a20693cfab6704cc7
Ratheshan03 <lionratheshan@gmail.com>
2025-01-16 12:01:34 +0530

    update database loading script with web scraping functionality

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,7 +1,107 @@
-import { DataAPIClient} from '@datastax/astra-db-ts'
-import {PupperteerWebBaseLoader} from "langchain/document_loaders/web/puppeteer";
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
 import OpenAI from "openai";
+
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
 import "dotenv/config";
 
+type SimilarityMetric = "dot_product" | "cosine" | "euclidean";
 
+const {
+  ASTRA_DB_NAMESPACE,
+  ASTRA_DB_COLLECTION,
+  ASTRA_DB_API_ENDPOINT,
+  ASTRA_DB_APPLICATION_TOKEN,
+  OPENAI_API_KEY,
+} = process.env;
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+// Define Websites to scrape
+const f1Data = [
+  "https://en.wikipedia.org/wiki/Formula_One",
+  "https://www.skysports.com/f1/news/12433/13117256/lewis-hamilton-says-move-to-ferrari-from-mercedes-doesnt-need-vindicating-amid-irritation-at-coverage",
+  "https://www.formula1.com/en/latest/all",
+  "https://www.forbes.com/sites/brettknight/2023/11/29/formula-1s-highest-paid-drivers-2023/",
+  "https://www.autosport.com/f1/news/history-of-female-f1-drivers-including-grand-prix-starters-and-test-drivers/10584871/",
+  "https://en.wikipedia.org/wiki/2023_Formula_One_World_Championship",
+  "https://en.wikipedia.org/wiki/2022_Formula_One_World_Championship",
+  "https://en.wikipedia.org/wiki/List_of_Formula_One_World_Drivers%27_Champions",
+  "https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship",
+  "https://www.formula1.com/en/results.html/2024/races.html",
+  "https://www.formula1.com/en/racing/2024.html",
+];
+
+// Connect to DB
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE });
+
+// Chunk Data
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 512,
+  chunkOverlap: 100,
+});
+
+// Create collection in the DB
+const createCollection = async (
+  similarityMetric: SimilarityMetric = "dot_product"
+) => {
+  const res = await db.createCollection(ASTRA_DB_COLLECTION, {
+    vector: {
+      dimension: 1536,
+      metric: similarityMetric,
+    },
+  });
+
+  console.log(res);
+};
+
+// Create Vector Embeddings
+const loadSampleData = async () => {
+  const collection = await db.collection(ASTRA_DB_COLLECTION);
+  for await (const url of f1Data) {
+    const content = await scrapePage(url);
+    const chunks = await splitter.splitText(content);
+    for await (const chunk of chunks) {
+      const embedding = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: chunk,
+        encoding_format: "float",
+      });
+
+      // Insert Data into the Database
+      //Insert each chunk as a vector with its embedding
+      const vector = embedding.data[0].embedding;
+
+      const response = await collection.insertOne({
+        $vector: vector,
+        text: chunk,
+      });
+
+      console.log(response);
+    }
+  }
+};
+
+// Define a function to scrape data from websites using Puppeteer
+const scrapePage = async (url: string) => {
+  const loader = new PuppeteerWebBaseLoader(url, {
+    launchOptions: {
+      headless: true,
+    },
+    gotoOptions: {
+      waitUntil: "domcontentloaded",
+    },
+    evaluate: async (page, browser) => {
+      const result = await page.evaluate(() => document.body.innerHTML);
+      await browser.close();
+      return result;
+    },
+  });
+
+  return (await loader.scrape())?.replace(/<[^>]*>?/gm, ""); // Remove HTML tags
+};
+
+// create the collection
+createCollection().then(() => loadSampleData());
